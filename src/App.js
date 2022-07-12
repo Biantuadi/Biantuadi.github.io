8e9932c38261ae03d9cdc0a2b8e49f410cf845e5
Kevin-beni <benibiantuadi@gmail.com>
2022-07-13 01:17:49 +0200

    ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
-import Loader from "./components/Loader";
+import Loader from "./components/Loader.Building";
 import Routes from "./routes";
 
 function App() {
   const [pageLoader, setPageLoader] = React.useState(true);
+
+  const action = "active";
+  if (action === "desactive") setPageLoader(false);
 
   return <>{pageLoader ? <Loader /> : <Routes />}</>;
 }
--- src/components/Loader.Building.jsx
+++ src/components/Loader.Building.jsx
@@ -3,7 +3,7 @@ import Video from "../assets/video/build.mp4";
 
 const Loader = () => {
   return (
-    <div class="page-loader">
+    <div className="page-loader">
       <video id="bgVideo" src={Video} autoPlay loop muted></video>
     </div> 
   );
