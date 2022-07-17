f8eb3a9e1cd93c0a928f0a4cade930850e40d92a
Kevin-beni <benibiantuadi@gmail.com>
2022-07-17 16:02:59 +0200

    ajout is open header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,34 @@ import React from "react";
 import Navigation from "./Navigation";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const open = React.useRef();
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <header>
+      <div className="header">
         <h1 className="h1">
           B<span className="span">iant</span>
         </h1>
-      <Navigation />
+        {isOpen ? (
+          <i className="fas fa-times" onClick={handleClick} />
+        ) : (
+          <i ref={open} className="fas fa-bars" onClick={handleClick}></i>
+        )}
+        <Navigation />
+      </div>
+
+      {isOpen && (
+        <>
+          <div className="isPhone">
+            <Navigation />
+          </div>
+        </>
+      )}
     </header>
   );
 };
